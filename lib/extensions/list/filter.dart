0a2e8d34fde9c9b4ba99cfc861c54e54154209df
John-Smith-Doe <john.smith_doe@yahoo.com>
2022-11-18 20:51:19 +0100

    chapter-35

---
+++ lib/extensions/list/filter.dart
@@ -0,0 +1,5 @@
+//for filtering out the stream in a List for T (some thing)
+extension Filter<T> on Stream<List<T>> {
+  Stream<List<T>> filter(bool Function(T) where) =>
+      map((items) => items.where(where).toList());
+}
--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:logger/logger.dart';
+import 'package:notes_app/extensions/list/filter.dart';
 import 'package:notes_app/services/logging.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
@@ -13,6 +14,7 @@ import 'crud_exceptions.dart';
 class NotesService {
   var log = logger(NotesService);
   Database? _db;
+  DatabaseUser? _user;
 
   //cache
   List<DatabaseNote> _notes = [];
@@ -31,16 +33,30 @@ class NotesService {
   late final StreamController<List<DatabaseNote>> _notesStreamController;
 
   //get all notes
-  Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
+  Stream<List<DatabaseNote>> get allNotes =>
+      _notesStreamController.stream.filter((note) {
+        final currentUser = _user;
+        if (currentUser != null) {
+          return note.userId == currentUser.id;
+        } else {
+          throw UserShouldBeSetBeforeReadingAllNotes();
+        }
+      });
 
-  Future<DatabaseUser> getOrCreateUser({required String email}) async {
+  Future<DatabaseUser> getOrCreateUser(
+      {required String email, bool setAsCurrentUser = true}) async {
     log.i('getOrCreateUser function is called');
-
     try {
       final user = await getUser(email: email);
+      if (setAsCurrentUser) {
+        _user = user;
+      }
       return user;
     } on CouldNotFindUser {
       final createdUser = await createUser(email: email);
+      if (setAsCurrentUser) {
+        _user = createdUser;
+      }
       log.i('createUser is $createdUser');
       return createdUser;
     } catch (e) {
