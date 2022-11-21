df857d321a96f5b1d6d6ad3aa29982736f22e567
John-Smith-Doe <john.smith_doe@yahoo.com>
2022-11-21 19:22:11 +0100

    chapter-38

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/services/auth/auth_service.dart';
 import 'package:notes_app/services/cloud/firebase_cloud_storage.dart';
 import 'package:notes_app/services/logging.dart';
+import 'package:notes_app/utilities/dialogs/cannot_share_empty_note_dialog.dart';
 import 'package:notes_app/utilities/generics/get_arguments.dart';
 import 'package:notes_app/services/cloud/cloud_note.dart';
 import 'package:notes_app/services/cloud/cloud_storage_exception.dart';
+import 'package:share_plus/share_plus.dart';
 
 class CreateUpdateNoteView extends StatefulWidget {
   const CreateUpdateNoteView({super.key});
@@ -114,6 +116,19 @@ class _NewNotesViewState extends State<CreateUpdateNoteView> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('New Note'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final text = _textEditingController.text;
+              if (_note == null || text.isEmpty) {
+                await showCannotShareEmptyNoteDialog(context);
+              } else {
+                Share.share(text);
+              }
+            },
+            icon: const Icon(Icons.share),
+          )
+        ],
       ),
       body: FutureBuilder(
         //when this createNewNote is finished return note
